b2b323ba8b571a3e5508291f6d5420058b20e7f1
Alvaro Monserrat <alvaro.monserrat@hotmail.com>
2016-12-27 18:28:39 -0300

    Actualizar foto

--- Project_RGB_SR300/Project_RGB_SR300/main.cpp
+++ Project_RGB_SR300/Project_RGB_SR300/main.cpp
@@ -140,7 +140,7 @@ int main() {
 	return 0;
 }
 
-
+//Evento click
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
 
 	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
